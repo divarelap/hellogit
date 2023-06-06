203ab68f7f0fe56edea38a3fe34db9d879d2c363
divarelap <divarelap@gmail.com>
2023-06-06 12:15:41 -0500

    hellogit.py jun/06/2023

--- hellogit.py
+++ hellogit.py
@@ -4,4 +4,9 @@ print('Hello Git')
 
 print('Editado el 06--2-2023')
 print('Editado :)')
-print('Editado jun/05/2023')
+
+# print('Editado jun/05/2023')
+
+print('New Hello Git with changes! 🕹')
+
+print('Elimino lo ingresado el Editado jun/05/2023')
